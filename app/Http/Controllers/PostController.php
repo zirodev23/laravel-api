ae44ffec455072716581c82ea2a34c89db00fa7d
unknown <your@email.example>
2024-09-20 14:29:42 +0300

    api crud logic added to controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -13,15 +12,22 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return Post::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post = Post::create($fields);
+
+        return $post;
     }
 
     /**
@@ -29,15 +35,22 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
-        //
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+
+        $post->update($fields);
+
+        return $post;
     }
 
     /**
@@ -45,6 +58,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return ['message' => "The post ($post->id) has been deleted"];
     }
 }
