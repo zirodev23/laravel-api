35bb7243397a2bab655a96d09b3ccec11aa8a1d3
unknown <your@email.example>
2024-09-20 13:05:10 +0300

    post resource routes added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::apiResource('posts', PostController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
