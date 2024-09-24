411372d00e6ec520fc7941058a2edfd864574067
zirodev <zirodev23@zirodev23.com>
2024-09-24 18:08:08 +0300

    auth controller added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        return 'register';
+    }
+
+    public function login(Request $request) {
+        return 'login';
+    }
+
+    public function logout(Request $request) {
+        return 'logout';
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
