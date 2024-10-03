08f3c5c3e33c5974f9628972489f4723b2720ae9
zirodev <zirodev23@zirodev23.com>
2024-10-04 00:31:13 +0300

    posts belong to a user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,8 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
