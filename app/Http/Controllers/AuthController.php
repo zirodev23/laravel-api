c93b634bfebe580df68f53938928fa8de5b4e82a
zirodev <zirodev23@zirodev23.com>
2024-10-03 23:54:23 +0300

    user can be registered

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        return 'register';
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+        
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
     }
 
     public function login(Request $request) {
