8e8cd2f865c70a32aa9c07d7bdd133e89a4c93a0
zirodev <zirodev23@zirodev23.com>
2024-10-04 17:07:48 +0300

    a post can be updated or deleted only by the user who owns it

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller implements HasMiddleware
 {
@@ -51,6 +52,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function update(Request $request, Post $post)
     {
+        Gate::authorize('modify', $post);
         $fields = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required'
@@ -66,6 +68,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function destroy(Post $post)
     {
+        Gate::authorize('modify', $post);
         $post->delete();
         return ['message' => "The post ($post->id) has been deleted"];
     }
