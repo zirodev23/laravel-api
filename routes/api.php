40b2187e4e42cb05da2a6f0fc6aff75f9d92bf5c
zirodev <zirodev23@zirodev23.com>
2024-10-04 17:16:00 +0300

    user instance returned using a token

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
 
